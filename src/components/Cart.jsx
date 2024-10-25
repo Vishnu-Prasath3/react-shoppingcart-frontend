ee32b85be45323952ac8d3eed67b06fabea79fa5
VishnuPrasath <eeevishnuprasath@gmail.com>
2024-10-25 18:18:11 +0530

    finalised

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,41 +1,41 @@
-import { products } from './Products';
-import '../components/cart.css';
-
-
-
+import { products } from "./Products";
+import "../components/cart.css";
 
 function Cart() {
-
-
-    function handleclick(index){
-    let newindex=products.map((p,ind)=>{ind!==index})
-    console.log(newindex);
-    
-    }
-
-
-    return (
-    <div className='box-model'>
-    {products && products.map((p,index)=>(
-        <div key={index} className=''>    
-        <div  className="card cardshadow" style={{width:"18rem"}}>
-    <img src={p.img} className="card-img-top" alt="product"></img>
-    <div className="card-body">
-      <h5 className="card-title">{p.name}</h5>
-      <p className="card-text ">{"Rs:"+p.price+" /."} </p>
-      <button   className="btn btn-primary" id='cartbtn' style={{}}  onClick={()=>{handleclick(index)}}>{"Cart"}</button>
-      <div id='addsub'>
-      <button>-</button>   <button>1</button>  <button>+</button>
-      </div>
+  let newid = [];
+  function handleclick(id) {
+    newid = products.map((p) => {
+      return p.id != id;
+    });
+    console.log(newid);
+  }
+  return (
+    <div className="box-model">
+      {products &&
+        products.map((p, index) => (
+          <div key={index} className="">
+            <div className="card cardshadow" style={{ width: "18rem" }}>
+              <img src={p.img} className="card-img-top" alt="product"></img>
+              <div className="card-body">
+                <h5 className="card-title">{p.name}</h5>
+                <p className="card-text">{"Rs:" + p.price + " /."}</p>
+                <button
+                  className={`btn btn-primary ${newid.map((a,index) =>index==false?'invisible':"")}`}
+                  onClick={() => {
+                    handleclick(index);
+                  }}
+                >
+                  {"Cart"}
+                </button>
+                <div id="addsub">
+                  <button>--</button> <button>1</button> <button>+</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
-    </div>
-    </div>
-    
-))}
-  </div>
+  );
+}
 
-  )
-    }
-
-
-export default Cart
+export default Cart;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "../components/hero.css";
 function Hero() {
   return (
